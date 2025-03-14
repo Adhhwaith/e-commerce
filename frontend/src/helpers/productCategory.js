4b53049f7858106b026bc86e6cfbf278a5d6d343
Adwaith <adwaiskumar@gmail.com>
2025-03-14 21:57:27 +0530

    changes

--- frontend/src/helpers/productCategory.js
+++ frontend/src/helpers/productCategory.js
@@ -1,16 +1,11 @@
 const productCategory = [
-    { id : 1, label : "Airpodes", value : "airpodes"},
-    { id : 2, label : "Camera", value : "camera"},
-    { id : 3, label : "Earphones", value : "earphones"},
-    { id : 4, label : "Mobiles", value : "mobiles"},
-    { id : 5, label : "Mouse", value : "Mouse"},
-    { id : 6, label : "Printers", value : "printers"},
-    { id : 7, label : "Processor", value : "processor"},
-    { id : 8, label : "Refrigerator", value : "refrigerator"},
-    { id : 9, label : "Speakers", value : "speakers"},
-    { id : 10, label : "Trimmers", value : "trimmers"},
-    { id : 11, label : "Televisions", value : "televisions"},
-    { id : 12, label : "Watches", value : "watches"},
+    { id : 1, label : "shirts", value : "shirts"},
+    { id : 2, label : "Popular's Watches", value : "Popular's Watches"},
+    { id : 3, label : "shoes", value : "shoes"},
+    { id : 4, label : "formal footwear", value : "formal footwear"},
+    { id : 5, label : "Perfume", value : "Perfume"},
+    { id : 6, label : "Pants", value : "Pants"},
+   
 ]
 
 
